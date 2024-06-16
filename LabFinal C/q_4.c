420ba1050a395425f2af23dfae6d0fb49f68ba0e
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 11:11:19 +0600

    Add files via upload

---
+++ LabFinal C/q_4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+      int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]); 
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        cnt++;
+    } 
+    printf("%d",cnt);
+    return 0;
+}
