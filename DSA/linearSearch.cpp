1597602f022df99542ab51ec34fac922bebf63d9
uzzal2200 <uzzalhasan1238@gmail.com>
2024-12-06 21:18:04 +0600

    uploaded

--- DSA/linearSearch.cpp
+++ DSA/linearSearch.cpp
@@ -1,28 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n;
-    cout << "Enter size of the linear array :";
+    cout << "Enter size of the linear array: ";
     cin >> n;
+
     int arr[n];
-   cout << "Enter elements of the array : ";
+    cout << "Enter elements of the array: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
     int target;
-    cout << "Enter the elemnt you want to find: ";
+    cout << "Enter the element you want to find: ";
     cin >> target;
+
+    int result = 0; // Flag variable to indicate if the element is found
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == target)
         {
-            cout << "Found at position " << i;
-            return 0;
+            cout << "Found at position " << i << endl;
+            result = 1; // Update the flag if the element is found
+            break;
         }
     }
-    cout << "ELement not found. ";
+
+    if (result == 0) // Check if the flag was updated
+    {
+        cout << "Element not found." << endl;
+    }
+
     return 0;
 }
